a99a170019747e97bcba0d2bbd8c105b78f13596
Savio Fonseca <savifon1@gmail.com>
2020-08-19 00:30:58 +0000

    exercício 01

--- main.cs
+++ main.cs
@@ -1,11 +1,23 @@
 using System;
 
 class MainClass {
-	public static void Teste () {
-    Console.WriteLine ("Imprime 'teste'");
+	public static void Teste (float numero) {
+    //Console.WriteLine ("O número informado foi " + numero);
+		Console.WriteLine ("O número informado foi {0} ", numero);
+
   }
 
   public static void Main () {
-    Teste();
+		float numero;
+		Console.Write ("Digite um valor: ");
+		numero = float.Parse(Console.ReadLine());
+    Teste(numero);
+
   }
 }
+
+/*
+CONDICIONAIS: if / else
+REPETIÇÕES: for / while
+ESTRUTURA DE DADOS PRIMITIVAS: (int, float, char, string) []-[,]
+*/
